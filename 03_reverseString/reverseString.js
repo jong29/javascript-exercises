0c06b072260ebc38187d755f8a0243cdd08daec4
jong29 <jongwoop97@gmail.com>
2023-02-24 20:34:59 +0900

    jse03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    ans = "";
+    for (let i=str.length-1; i>-1; i--) {
+        ans += str[i];
+    }
+    return ans;
 };
 
 // Do not edit below this line
