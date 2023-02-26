f58baa10da1dbab1743dcf1785b2fa0873909f94
jong29 <jongwoop97@gmail.com>
2023-02-26 22:27:35 +0900

    jse04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...theArgs) {
+    for (let i=0; i<theArgs.length; i++) {
+        let id = arr.indexOf(theArgs[i]);
+        if (id != -1){
+            arr.splice(id, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
